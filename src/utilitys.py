4275537041e2b16a05caf1f70efaf91346ad80d3
pratebalo <pablixu_4@hotmail.com>
2023-10-22 16:58:31 +0200

    .

--- src/utilitys.py
+++ src/utilitys.py
@@ -6,6 +6,11 @@ import re
 
 ID_ADMIN = config("ID_ADMIN")
 ID_LOGS = config("ID_LOGS")
+ID_SHEET_CASTORES = config("ID_SHEET_CASTORES")
+ID_SHEET_MANADA = config("ID_SHEET_MANADA")
+ID_SHEET_TROPA = config("ID_SHEET_TROPA")
+ID_SHEET_ESCULTAS = config("ID_SHEET_ESCULTAS")
+ID_SHEET_ROVER = config("ID_SHEET_ROVER")
 
 
 def get_person(person_id: int):
@@ -28,12 +33,12 @@ async def check_log_errors(context: ContextTypes.DEFAULT_TYPE):
     result = re.split(regex, logs)
     result = [element for element in result if element]
     if "last_error_log" in context.bot_data.keys():
-        diff = result[result.index(context.bot_data["last_error_log"]) + 1:]
-        for text in diff:
-            await context.bot.sendMessage(ID_LOGS, text=f"MANITOBA - {text}")
-        context.bot_data["last_error_log"] = result[-1]
-    else:
-        context.bot_data["last_error_log"] = result[-1]
+        if context.bot_data["last_error_log"] in result:
+            diff = result[result.index(context.bot_data["last_error_log"]) + 1:]
+            for text in diff:
+                await context.bot.sendMessage(ID_LOGS, text=f"MANITOBA - {text}")
+
+    context.bot_data["last_error_log"] = result[-1]
 
 
 async def check_last_logs(context: ContextTypes.DEFAULT_TYPE):
@@ -44,9 +49,9 @@ async def check_last_logs(context: ContextTypes.DEFAULT_TYPE):
     result = re.split(regex, logs)
     result = [element for element in result if element]
     if "last_log" in context.bot_data.keys():
-        diff = result[result.index(context.bot_data["last_log"]) + 1:]
-        for text in diff:
-            await context.bot.sendMessage(ID_LOGS, text=f"MANITOBA - {text}")
-        context.bot_data["last_log"] = result[-1]
-    else:
-        context.bot_data["last_log"] = result[-1]
+        if context.bot_data["last_log"] in result:
+            diff = result[result.index(context.bot_data["last_log"]) + 1:]
+            for text in diff:
+                await context.bot.sendMessage(ID_LOGS, text=f"MANITOBA - {text}")
+
+    context.bot_data["last_log"] = result[-1]
