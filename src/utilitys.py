7dab7655e1c88d7100bc307a6d91bad56ea9fd78
pratebalo <pablixu_4@hotmail.com>
2023-12-20 00:32:22 +0100

    .

--- src/utilitys.py
+++ src/utilitys.py
@@ -33,7 +33,7 @@ async def check_log_errors(context: ContextTypes.DEFAULT_TYPE):
 
     regex = r'\d{4}-\d{2}-\d{2} '
     result = re.split(regex, logs)
-    result = [element for element in result if element]
+    result = [element.strip() for element in result if element]
     if "last_error_log" in context.bot_data.keys():
         if context.bot_data["last_error_log"] in result:
             diff = result[result.index(context.bot_data["last_error_log"]) + 1:]
@@ -51,7 +51,7 @@ async def check_last_logs(context: ContextTypes.DEFAULT_TYPE):
 
     regex = r'\d{4}-\d{2}-\d{2} '
     result = re.split(regex, logs)
-    result = [element for element in result if element]
+    result = [element.strip() for element in result if element]
     if "last_log" in context.bot_data.keys():
         if context.bot_data["last_log"] in result:
             diff = result[result.index(context.bot_data["last_log"]) + 1:]
@@ -65,22 +65,31 @@ async def check_last_logs(context: ContextTypes.DEFAULT_TYPE):
 
 
 def get_last_lines(file, num_lines=100):
+    buffer_size = 4096  # Puedes ajustar el tamaño del búfer según tus necesidades
+
     with open(file, 'rb') as f:
         f.seek(0, 2)
         pos = f.tell()
         total_lines = 0
         lines = []
 
-        while pos > 0 and total_lines < num_lines:
-            pos -= 1
+        while pos > 0 and 10 < num_lines:
+            to_read = min(pos, buffer_size)
+            pos -= to_read
             f.seek(pos)
-            char = f.read(1)
+            chunk = f.read(to_read)
 
-            if char == b'\n':
-                total_lines += 1
-            lines.insert(0, char.decode('utf-8', errors='replace'))
+            # Decodificar el bloque de bytes y dividir por líneas
+            lines_in_chunk = chunk.decode('utf-8', errors='replace').splitlines()
 
-        return ''.join(lines)
+            # Contar las líneas en el bloque y actualizar el contador total
+            total_lines += len(lines_in_chunk) - 1
+
+            # Insertar las líneas en la lista (excepto la última, que puede estar incompleta)
+            lines[:0] = lines_in_chunk[:-1]
+
+        # Devolver las líneas concatenadas
+        return '\n'.join(lines[-num_lines:])
 
 # folders = [ID_FOLDER_CASTORES, ID_FOLDER_MANADA, ID_FOLDER_TROPA, ID_FOLDER_ESCULTAS, ID_FOLDER_ROVER, ID_FOLDER_KRAAL]
 # for i in range(1, 6):
